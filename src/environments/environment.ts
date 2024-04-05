da33158c1b6059c177c11aa86a187cd9f2904d9a
Natyoverwatch <natap455@gmail.com>
2024-04-04 19:37:47 -0500

    actualizacion archivos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,6 @@
 // las IP o EndPoint de las API que voy a consumir
 
 export const environment = {
-    production: false,
-<<<<<<< HEAD
-    base_url: "https://backendprojecto.onrender.com/api/v1",
-=======
-    base_url: "http://localhost:4000/api/v1",
->>>>>>> db0bf76b35262a94f152f82a251a33d5b088837d
+  production: false,
+  base_url: 'http://localhost:4000/api/v1',
 };
